514b1e31becb2d37c4ff09f54347d7da297647ae
khilchand <khilchandkapade76@gmail.com>
2025-09-15 13:24:00 +0530

    interface

---
+++ src/com/project/Lab_Basic.java
@@ -0,0 +1,60 @@
+package com.project;
+
+public class Lab_Basic {
+	byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		//primitive type conversion
+//		    int i = 100;
+//	        byte b = (byte) i;
+//	        float f = 123.45f;
+//	        int Float = (int) f;
+//	        double d = 456.789;
+//	        long l = (long) d;
+//	        char c = (char) i;
+//	        short s = (short) l;
+//
+//	        System.out.println("int to byte: " + b);
+//	        System.out.println("float to int: " + Float);
+//	        System.out.println("double to long: " + l);
+//	        System.out.println("int to char: " + c);
+//	        System.out.println("long to short: " + s);
+//	        
+//	       
+//	        // concatenation 
+//	        String firstName = "John";
+//	        String lastName = "Doe";
+//	        String city = "New York";
+//
+//	        String sentence = firstName + " " + lastName + " lives in " + city + ".";
+//	        System.out.println(sentence);
+
+	        
+	        //
+		
+	    Lab_Basic obj = new Lab_Basic();
+        System.out.println("byte: " + obj.b);
+        System.out.println("short: " + obj.s);
+        System.out.println("int: " + obj.i);
+        System.out.println("long: " + obj.l);
+        System.out.println("float: " + obj.f);
+        System.out.println("double: " + obj.d);
+        System.out.println("char: '" + obj.c + "'");
+        System.out.println("boolean: " + obj.bool);
+
+
+	        
+
+
+	}
+
+}
